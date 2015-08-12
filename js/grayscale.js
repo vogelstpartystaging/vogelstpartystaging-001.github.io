871b528d781790724f892893d51b77da713353a9
haszari <haszari@cartoonbeats.com>
2015-08-12 20:04:23 +1200

    vogelly tweaks

--- js/grayscale.js
+++ js/grayscale.js
@@ -41,7 +41,7 @@ function init() {
         zoom: 15,
 
         // The latitude and longitude to center the map (always required)
-        center: new google.maps.LatLng(40.6700, -73.9400), // New York
+        center: new google.maps.LatLng(-45.878982, 170.503525), // Vogel Street/Water Street
 
         // Disables the default Google Maps UI components
         disableDefaultUI: true,
